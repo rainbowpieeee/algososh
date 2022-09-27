862ab0e4110b07ac4eb854dbd38e065d1967d69f
rainbowpieeee <catherinestolbova@yandex.ru>
2022-09-28 02:33:50 +0500

    Создана разметка и стили для страницы Строка

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import { FC } from "react";
+import styles from "./string.module.css";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Circle } from "../ui/circle/circle";
 
-export const StringComponent: React.FC = () => {
+export const StringComponent: FC = () => {
+  const handleClick = () => {
+    alert("Изменить текст");
+  };
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.wrapper}>
+        <Input extraClass={styles.input} isLimitText={true} maxLength={11} />
+        <Button text={"Развернуть"} onClick={handleClick} />
+      </div>
+      <div className={styles.wrapper}>
+        <ul className={styles.list}>
+          <li className={`${styles["list-element"]}`}>
+            <Circle letter={"A"} />
+          </li>
+          <li className={`${styles["list-element"]}`}>
+            <Circle letter={"B"} />
+          </li>
+          <li className={`${styles["list-element"]}`}>
+            <Circle letter={"R"} />
+          </li>
+          <li className={`${styles["list-element"]}`}>
+            <Circle letter={"A"} />
+          </li>
+          <li className={`${styles["list-element"]}`}>
+            <Circle letter={"K"} />
+          </li>
+        </ul>
+      </div>
     </SolutionLayout>
   );
 };
