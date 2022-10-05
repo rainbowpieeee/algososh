7aa8c30aec78ad1e7c64cc69e10ca93eb5f9c822
rainbowpieeee <catherinestolbova@yandex.ru>
2022-10-06 01:43:49 +0500

    Добавлена базовая логика страницы Сортировки

--- src/pages/sorting/sorting.tsx
+++ src/pages/sorting/sorting.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./sorting.module.css";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
 import { RadioInput } from "../../components/ui/radio-input/radio-input";
@@ -6,31 +6,68 @@ import { Button } from "../../components/ui/button/button";
 import { Column } from "../../components/ui/column/column";
 import { Direction } from "../../types/direction";
 import InputWrapper from "../../components/input-wrapper/input-wrapper";
+import { ElementStates } from "../../types/element-states";
+
+interface IColumn {
+  number: number;
+  state: ElementStates;
+}
 
 export const SortingPage: FC = () => {
+  const [sortingType, setSortingType] = useState<string>("selection");
+  const [initialArr, setInitialArr] = useState<Array<IColumn>>([
+    { number: 2, state: ElementStates.Default },
+    { number: 34, state: ElementStates.Default },
+    { number: 17, state: ElementStates.Default },
+    { number: 100, state: ElementStates.Default },
+    { number: 50, state: ElementStates.Default },
+  ]);
+
   const generateNewArray = () => {
     alert("Сгенерировать новый массив");
+  };
+
+  const bubbleSorting = () => {
+    alert("Пузырек");
+  };
+
+  const selectionSorting = () => {
+    alert("Выбор");
   };
 
   return (
     <SolutionLayout title="Сортировка массива">
       <InputWrapper>
         <div className={styles.radioWrapper}>
-          <RadioInput label="Выбор" />
-          <RadioInput label="Пузырёк" />
+          <RadioInput
+            label="Выбор"
+            value="selection"
+            onChange={() => setSortingType("selection")}
+            checked={sortingType === "selection"}
+          />
+          <RadioInput
+            label="Пузырёк"
+            value="bubble"
+            onChange={() => setSortingType("bubble")}
+            checked={sortingType === "bubble"}
+          />
         </div>
         <div className={styles.buttonsWrapper}>
           <Button
             sorting={Direction.Ascending}
             type="submit"
             text={"По возрастанию"}
-            onClick={() => null}
+            onClick={() =>
+              sortingType === "selection" ? selectionSorting() : bubbleSorting()
+            }
           />
           <Button
             sorting={Direction.Descending}
             type="submit"
             text={"По убыванию"}
-            onClick={() => null}
+            onClick={() =>
+              sortingType === "selection" ? selectionSorting() : bubbleSorting()
+            }
           />
         </div>
         <Button
@@ -40,11 +77,14 @@ export const SortingPage: FC = () => {
         />
       </InputWrapper>
       <ul className={styles.list}>
-        <Column index={2} />
-        <Column index={34} />
-        <Column index={17} />
-        <Column index={100} />
-        <Column index={50} />
+      {initialArr &&
+          initialArr.map((el, index) => {
+            return (
+              <li key={index}>
+                <Column index={el.number} state={el.state} />
+              </li>
+            );
+          })}
       </ul>
     </SolutionLayout>
   );
