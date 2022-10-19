a11da5e5be440b9d6c90857f33f4b4ea518a1765
rainbowpieeee <catherinestolbova@yandex.ru>
2022-10-20 01:37:37 +0500

    Откорректированы функции сортировки по возрастанию и убыванию в странице сортировка, добавлена логика и рендер страницы stack

--- src/pages/sorting/sorting.tsx
+++ src/pages/sorting/sorting.tsx
@@ -33,17 +33,39 @@ export const SortingPage: FC = () => {
     processSetter(true);
     const arr = [...initialArr];
 
-    for (let i = 0; i < arr.length - 1; i++) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log("i", i);
+      console.log("arr i", arr);
       for (let j = 0; j < arr.length - i - 1; j++) {
+        console.log("j", j);
+        console.log("arr j", arr);
+        // выделяем элементы, которые будем сравнивать
+        arr[j].state = ElementStates.Changing;
+        arr[j + 1].state = ElementStates.Changing;
         initialArrSetter([...arr]);
         await setDelay(SHORT_DELAY_IN_MS);
-        if (arr[j].number > arr[j + 1].number) {
+        if (
+          (sortingOption === "ascending" ? arr[j].number : arr[j + 1].number) >
+          (sortingOption === "ascending" ? arr[j + 1].number : arr[j].number)
+        ) {
           initialArrSetter([...arr]);
           swapArrElements(arr, j, j + 1);
+          await setDelay(SHORT_DELAY_IN_MS);
+        }
+        // убираем розовую подсветку после сравнения элементов и возможного свопа
+        arr[j].state = ElementStates.Default;
+        arr[j + 1].state = ElementStates.Default;
+        // красим в зеленый последний во внутреннем цикле и в итоге отсортированный
+        if (j === arr.length - i - 2) {
+          arr[j + 1].state = ElementStates.Modified;
         }
       }
+      //прошли весь внешний цикл и красим оставшийся элемент в зеленый
+      if (i === arr.length - 1) {
+        arr[0].state = ElementStates.Modified;
+      }
     }
-    //initialArrSetter([...arr]);
+    initialArrSetter([...arr]);
     processSetter(false);
   };
 
@@ -58,16 +80,44 @@ export const SortingPage: FC = () => {
     console.log("arr", arr);
     for (let i = 0; i < arr.length - 1; i++) {
       let swapInd = i;
-      console.log("arr", arr);
-      console.log("swapInd i", swapInd);
+      console.log("arr в первом цикле", arr);
+      // await setDelay(SHORT_DELAY_IN_MS);
+
       for (let j = i + 1; j < arr.length; j++) {
-        if (arr[swapInd].number < arr[j].number) {
+        console.log("swapInd i", swapInd);
+        console.log("arr[swapInd] розовым", arr[swapInd].number);
+        console.log("j", j);
+        console.log("arr[j] розовым", arr[j].number);
+        //подсвечиваем розовым два сравниваемых элемента
+        arr[swapInd].state = ElementStates.Changing;
+        await setDelay(SHORT_DELAY_IN_MS);
+        arr[j].state = ElementStates.Changing;
+        if (
+          (sortingOption === "ascending"
+            ? arr[swapInd].number
+            : arr[j].number) >
+          (sortingOption === "ascending" ? arr[j].number : arr[swapInd].number)
+        ) {
           swapInd = j;
-          console.log("swapInd j", swapInd);
+          console.log("меняем в условии swapInd с i на j", swapInd);
           initialArrSetter([...arr]);
           console.log("arr in j", arr);
           await setDelay(SHORT_DELAY_IN_MS);
+        } else {
+          // если нет, то убираем выделение и выделяем слудующий
+
+          arr[j].state = ElementStates.Default;
+          console.log(
+            "если arr[swapInd].number < arr[j].number arr[j] в синий не меняем",
+            arr[j].number
+          );
         }
+        console.log("swapInd после условия в цикле j", swapInd);
+        console.log("arr[swapInd] зеленым", arr[swapInd].number);
+        arr[swapInd].state = ElementStates.Modified;
+        /*if (i === arr.length - 2 || j === arr.length - 1) {
+          arr[arr.length - 1].state = ElementStates.Modified;
+        }*/
       }
       if (swapInd !== i) {
         swapArrElements(arr, swapInd, i);
@@ -76,7 +126,7 @@ export const SortingPage: FC = () => {
         await setDelay(SHORT_DELAY_IN_MS);
       }
     }
-
+    initialArrSetter([...arr]);
     processSetter(false);
   };
 
--- src/pages/stack/stack.tsx
+++ src/pages/stack/stack.tsx
@@ -1,12 +1,125 @@
-import React from "react";
+import { FC, useState, FormEvent, useMemo } from "react";
 import styles from "./stack.module.css";
 import InputWrapper from "../../components/input-wrapper/input-wrapper";
 import { Button } from "../../components/ui/button/button";
 import { Circle } from "../../components/ui/circle/circle";
 import { Input } from "../../components/ui/input/input";
 import { SolutionLayout } from "../../components/ui/solution-layout/solution-layout";
+import { ElementStates } from "../../types/element-states";
+import { setDelay } from "../../utils/utils";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
-export const StackPage: React.FC = () => {
+export interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+  getSize: () => number;
+  clear: () => void;
+}
+
+export class Stack<T> implements IStack<T> {
+  container: T[] = [];
+
+  push = (item: T): void => {
+    this.container.push(item);
+  };
+
+  pop = (): void => {
+    this.container.pop();
+  };
+
+  peak = (): T | null => {
+    if (this.container.length !== 0) {
+      return this.container[this.container.length - 1];
+    }
+    return null;
+  };
+
+  getSize = () => this.container.length;
+
+  clear = () => (this.container.length = 0);
+}
+
+export interface ICircleElement {
+  adding?: boolean;
+  deleting?: boolean;
+  withoutArrow?: boolean;
+  tail?: string;
+  head?: string;
+  char?: string | null;
+  extraCircle?: {
+    char: string;
+  };
+  state: ElementStates;
+}
+
+export const StackPage: FC = () => {
+  const stack = useMemo(() => new Stack<string>(), []);
+
+  const [inputValue, setInputValue] = useState<string>("");
+  const [elementsArr, setElementsArr] = useState<ICircleElement[]>([]);
+  const [isPushing, setIsPushing] = useState(false);
+  const [isPopping, setIsPopping] = useState(false);
+
+  const clear = () => {
+    stack.clear();
+    setElementsArr([]);
+  };
+
+  const pushElement = async () => {
+    //лочим кнопки
+    setIsPushing(true);
+    //сохраняем новый стек
+    stack.push(inputValue);
+    console.log("stack", stack);
+    setInputValue("");
+    //сбрасываем все настройки цвета, топ в стейте
+    elementsArr.forEach((el) => {
+      el.state = ElementStates.Default;
+      el.head = "";
+    });
+    // достать последний запушеный элемент из стека и добавить его в стейт
+    const pushedElement = stack.peak();
+    elementsArr.push({
+      char: pushedElement ? pushedElement : "",
+      state: ElementStates.Default,
+    });
+    setElementsArr([...elementsArr]);
+    await setDelay(SHORT_DELAY_IN_MS);
+    //Изменить стейт головы и сделать отментку top, подсветка розовым на время задержки
+    elementsArr[elementsArr.length - 1].head = "top";
+    elementsArr[elementsArr.length - 1].state = ElementStates.Changing;
+    setElementsArr([...elementsArr]);
+    await setDelay(SHORT_DELAY_IN_MS);
+    elementsArr[elementsArr.length - 1].state = ElementStates.Default;
+    setElementsArr([...elementsArr]);
+    //разблокируем кнопки
+    setIsPushing(false);
+  };
+
+  const popElement = async () => {
+    //лочим кнопки
+    setIsPopping(true);
+    //удаляем элемент из стэка
+    stack.pop();
+    console.log("stack", stack);
+    console.log("stack размер", stack.getSize());
+    // проверка на пустоту стэка
+    if (stack.getSize()) {
+      elementsArr.pop();
+      setElementsArr([...elementsArr]);
+      await setDelay(SHORT_DELAY_IN_MS);
+      //Изменить стейт головы и сделать отментку top
+      elementsArr[elementsArr.length - 1].head = "top";
+      elementsArr[elementsArr.length - 1].state = ElementStates.Changing;
+      setElementsArr([...elementsArr]);
+    } else {
+      setElementsArr([]);
+    }
+    //разблокируем кнопки
+    setIsPopping(false);
+  };
+
   return (
     <SolutionLayout title="Стек">
       <InputWrapper>
@@ -14,30 +127,49 @@ export const StackPage: React.FC = () => {
           extraClass={styles.input}
           placeholder="Введите текст"
           isLimitText={true}
+          min={1}
           maxLength={4}
+          value={inputValue || ""}
+          onChange={(e: FormEvent<HTMLInputElement>) =>
+            setInputValue(e.currentTarget.value)
+          }
+          disabled={isPushing || isPopping}
         />
-        <Button text="Добавить" type="button" onClick={() => null} />
-        <Button text="Удалить" type="button" onClick={() => null} />
+        <Button
+          text="Добавить"
+          type="button"
+          onClick={() => pushElement()}
+          isLoader={isPushing}
+          disabled={!inputValue || isPopping || elementsArr.length > 12}
+        />
+        <Button
+          text="Удалить"
+          type="button"
+          onClick={() => popElement()}
+          isLoader={isPopping}
+          disabled={!elementsArr.length || isPushing}
+        />
         <Button
           extraClass={styles.resetButton}
           text="Очистить"
           type="button"
-          onClick={() => null}
+          disabled={!elementsArr.length || isPopping || isPushing}
+          onClick={() => clear()}
         />
       </InputWrapper>
       <ul className={styles.list}>
-        <li>
-          <Circle letter={"A"} index={0} />
-        </li>
-        <li>
-          <Circle letter={"A"} index={1} />
-        </li>
-        <li>
-          <Circle letter={"A"} index={2} />
-        </li>
-        <li>
-          <Circle letter={"A"} index={3} />
-        </li>
+      {elementsArr.map((char, index) => {
+          return (
+            <li key={index}>
+              <Circle
+                head={char.head}
+                state={char.state}
+                index={index}
+                letter={char.char}
+              />
+            </li>
+          );
+        })}
       </ul>
     </SolutionLayout>
   );
